21f3c5d70b210d60a2231b2ff541769e76788b82
Shashank Raghuraj <shashankdoestennis@gmail.com>
2020-02-26 19:21:49 -0500

    Restaurant Class

---
+++ Restaurant Class Activity/Restaurant Class Activity/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Restaurant_Class_Activity
+{
+    class Restaurant
+    {
+        public string NameOfFood;
+        public double PriceOfFood;
+        public int quantity;
+
+        public void Menu()
+        {
+            MessageBox.Show("You have bought " + NameOfFood + " It costs " + PriceOfFood + " you have ordered a quantity of " +  quantity);
+        } 
+    }
+}
