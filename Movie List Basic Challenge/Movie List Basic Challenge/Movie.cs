c3971f3ea91cecdb601e97a19670e843614690c7
Shashank Raghuraj <shashankdoestennis@gmail.com>
2020-03-04 18:57:45 -0500

    New Changes for Movis

---
+++ Movie List Basic Challenge/Movie List Basic Challenge/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Movie_List_Basic_Challenge
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+
+        public Movie()
+        {
+
+        }
+
+        public Movie(string myTitle, int myReleaseYear)
+        {
+            this.Title = myTitle;
+            this.ReleaseYear = myReleaseYear;
+        }
+
+        public void ShowDetails()
+        {
+            string info = "Ttile: " + this.Title;
+            info += "\nRelease Year:" + this.ReleaseYear;
+            MessageBox.Show(info);
+        }
+    }
+}
